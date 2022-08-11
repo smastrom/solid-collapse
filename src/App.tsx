6116537412874a28594138b28a4eaef11667b6a0
smastrom <78644132+smastrom@users.noreply.github.com>
2022-08-11 13:08:33 +0200

    feat: add collapse file, basic readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,63 @@
-import { Component, createEffect, createSignal, on, onMount } from "solid-js";
+import { Component, createEffect, createSignal, on, onMount } from 'solid-js';
 
-import styles from "./App.module.css";
+import styles from './App.module.css';
+
+import { a11y } from './a11y';
+import { Collapse } from './Collapse';
+
+type Props = {
+	class: string;
+};
 
 const App: Component = () => {
-    const [isOpen, setIsOpen] = createSignal(false);
-    let element: HTMLDivElement;
+	const [isOpen, setIsOpen] = createSignal(false);
+	const [isOpen2, setIsOpen2] = createSignal(false);
 
-    onMount(() => {
-        element.style.height = "0";
-        element.style.overflow = "hidden";
-    });
+	// let element: HTMLDivElement; // if same component var name, creates issue
 
-    createEffect(
-        on(
-            isOpen,
-            () => {
-                element.style.display = "";
-                element.style.height = `${element.scrollHeight}px`;
-                if (isOpen() === false) {
-                    setTimeout(() => {
-                        console.log("Ciao!");
-                        element.style.height = "0";
-                        element.style.overflow = "hidden";
-                    });
-                }
-            },
-            { defer: true }
-        )
-    );
+	return (
+		<div class={styles.App}>
+			<div>{`${isOpen()}`}</div>
 
-    const handleTransitionEnd = () => {
-        if (isOpen()) {
-            element.style.height = "";
-            element.style.overflow = "";
-            console.log("Just Opened");
-        } else {
-            element.style.display = "none";
-        }
-    };
+			<button onClick={() => setIsOpen(!isOpen())}>Open / Close</button>
 
-    return (
-        <div class={styles.App}>
-            <div>{`${isOpen()}`}</div>
+			<header class={styles.header}>
+				<div style="max-width: 600px; padding: 20px">
+					<Collapse signal={isOpen} class="collapseTransition">
+						Phasellus sit amet bibendum mauris. Aliquam in euismod leo, eu posuere ipsum. Class
+						aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+						himenaeos. Nunc eleifend nunc tortor, id bibendum mi rutrum vel. Nulla id tortor
+						posuere, placerat urna at, tincidunt justo. Vivamus interdum, velit interdum
+						hendrerit pharetra, lorem leo tincidunt ex, id varius odio lacus ut risus. Maecenas
+						urna urna, tempus eget varius vitae, dapibus eu tortor. Sed venenatis id orci vitae
+						suscipit. Fusce porta, lectus quis consequat eleifend, lacus neque congue purus,
+						eget suscipit dolor arcu nec est. Morbi vitae sollicitudin sem. Nulla condimentum
+						orci vitae placerat rhoncus. Integer suscipit id metus et cursus. Aenean a gravida
+						urna, at porta ex.
+					</Collapse>
+				</div>
+			</header>
 
-            <button onClick={() => setIsOpen((isOpen) => !isOpen)}>Open / Close</button>
-            <header class={styles.header}>
-                <div style="max-width: 600px; padding: 20px">
-                    <div
-                        class="collapseTransition"
-                        onTransitionEnd={handleTransitionEnd}
-                        ref={element}
-                    >
-                        Phasellus sit amet bibendum mauris. Aliquam in euismod leo, eu posuere
-                        ipsum. Class aptent taciti sociosqu ad litora torquent per conubia
-                        nostra, per inceptos himenaeos. Nunc eleifend nunc tortor, id bibendum
-                        mi rutrum vel. Nulla id tortor posuere, placerat urna at, tincidunt
-                        justo. Vivamus interdum, velit interdum hendrerit pharetra, lorem leo
-                        tincidunt ex, id varius odio lacus ut risus. Maecenas urna urna, tempus
-                        eget varius vitae, dapibus eu tortor. Sed venenatis id orci vitae
-                        suscipit. Fusce porta, lectus quis consequat eleifend, lacus neque
-                        congue purus, eget suscipit dolor arcu nec est. Morbi vitae
-                        sollicitudin sem. Nulla condimentum orci vitae placerat rhoncus.
-                        Integer suscipit id metus et cursus. Aenean a gravida urna, at porta
-                        ex.
-                    </div>
-                </div>
-            </header>
-        </div>
-    );
+			<button onClick={() => setIsOpen2(!isOpen2())}>Open / Close</button>
+
+			<header class={styles.header}>
+				<div style="max-width: 600px; padding: 20px">
+					<Collapse signal={isOpen2} class="collapseTransition">
+						Phasellus sit amet bibendum mauris. Aliquam in euismod leo, eu posuere ipsum. Class
+						aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+						himenaeos. Nunc eleifend nunc tortor, id bibendum mi rutrum vel. Nulla id tortor
+						posuere, placerat urna at, tincidunt justo. Vivamus interdum, velit interdum
+						hendrerit pharetra, lorem leo tincidunt ex, id varius odio lacus ut risus. Maecenas
+						urna urna, tempus eget varius vitae, dapibus eu tortor. Sed venenatis id orci vitae
+						suscipit. Fusce porta, lectus quis consequat eleifend, lacus neque congue purus,
+						eget suscipit dolor arcu nec est. Morbi vitae sollicitudin sem. Nulla condimentum
+						orci vitae placerat rhoncus. Integer suscipit id metus et cursus. Aenean a gravida
+						urna, at porta ex.
+					</Collapse>
+				</div>
+			</header>
+		</div>
+	);
 };
 
 export default App;

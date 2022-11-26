d6adbd4a7bd0d1040d791b9ee40c7e28163a1028
smastrom <78644132+smastrom@users.noreply.github.com>
2022-11-26 22:18:59 +0100

    edit tests

--- cypress/e2e/Collapse.cy.ts
+++ cypress/e2e/Collapse.cy.ts
@@ -1,12 +1,16 @@
 const CYPRESS_URL = Cypress.env('CYPRESS_URL');
 
+const TOGGLE = '#single_btn';
+const COLLAPSE = '#single_collapse';
+
 describe('CSS properties', () => {
 	beforeEach(() => {
 		cy.visit(CYPRESS_URL);
 		cy.viewport('macbook-15');
 	});
-	it('Should have default styles if collapsed on mount', () => {
-		cy.get('#single_collapse')
+
+	it('Should have correct styles when collapsed', () => {
+		cy.get(COLLAPSE)
 			.should('have.css', 'display', 'none')
 			.and('have.css', 'padding', '0px')
 			.and('not.have.css', 'transition', '')
@@ -17,6 +21,35 @@ describe('CSS properties', () => {
 				 */
 				expect(getComputedStyle(element[0]).height).to.eq('auto');
 			});
+	});
+
+	it('Should have correct styles when expanded', () => {
+		cy.get(TOGGLE).click();
+		cy.get(COLLAPSE)
+			.should('have.css', 'padding', '0px')
+			.and('not.have.css', 'transition', '')
+			.and('not.have.css', 'display', 'none')
+			.and('not.have.css', 'overflow', 'hidden');
+	});
+
+	it('Should change height if resizing on expanded', () => {
+		cy.get(TOGGLE).click();
+		cy.wait(300);
+
+		for (let i = 0; i < 20; i++) {
+			cy.get(COLLAPSE)
+				.invoke('height')
+				.then((desktopHeight) => {
+					cy.viewport('iphone-x');
+					cy.wait(300);
+					cy.get('#single_collapse').invoke('height').should('be.greaterThan', desktopHeight);
+				})
+				.then((mobileHeight) => {
+					cy.viewport('macbook-13');
+					cy.wait(300);
+					cy.get('#single_collapse').invoke('height').should('be.lessThan', mobileHeight);
+				});
+		}
 	});
 });
 
